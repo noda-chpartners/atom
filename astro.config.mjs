c1cf23bd0c45a11e95915ce5afbdc8d51585203e
Mode-s <shota.umemoto7@gmail.com>
2026-09-23 19:20:24 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-const site = process.env.SITE_URL;
-
 // https://astro.build/config
 export default defineConfig({
-	...(site ? { site } : {}),
+	site: "https://atom-2v0.pages.dev",
 });
